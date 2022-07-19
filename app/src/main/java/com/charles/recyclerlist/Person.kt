326843dfb76aab7aad0228303058e6d56d0a3ac8
lyricalcoder16 <charlesochanda16@gmail.com>
2022-07-19 12:14:02 +0300

    RecyclerList

---
+++ app/src/main/java/com/charles/recyclerlist/Person.kt
@@ -0,0 +1,10 @@
+package com.charles.recyclerlist
+
+data class Person(
+    val name: String,
+    val email: String,
+    val address: String,
+    val age: Int,
+    val phone: String
+)
+//order must be maintained
